c7d5c3c3c37828bf71469376ec118fd9a022663b
Nguyễn Minh Hoàng <45387804+hohoang993007@users.noreply.github.com>
2019-12-24 17:42:46 +0700

    Update make_btree_soundex.c

--- convert_file_dict_to_btree_file/make_btree_soundex.c
+++ convert_file_dict_to_btree_file/make_btree_soundex.c
@@ -9,11 +9,12 @@ void make_btree_soundex(BTA*btree)
 {//phân phối lại các từ vào các nhóm-các gốc theo soundex_code
   btpos(btree,1);//đưa con trỏ btree về vị trí before the first key in the index of file btree input
   BTA*btree_soundex=btopn("./media/hoang/TONG HOP/C-Advance/PROJECT/Team/Main file_not_use_GTK/Dict_soundex.dat",0,0);
-  if(btree_soundex==NULL) btree_soundex=btcrt("./media/hoang/TONG HOP/C-Advance/PROJECT/Team/Main file_not_use_GTK/Dict_soundex.dat",0,0);
+  if(btree_soundex==NULL) 
+    btree_soundex=btcrt("./media/hoang/TONG HOP/C-Advance/PROJECT/Team/Main file_not_use_GTK/Dict_soundex.dat",0,0);
 
   char word[WORD_LENGTH];
-  char mean[1];//ko cần dùng đến mean nên khai báo nó nên cắt bớt để tiết kiệm bộ nhớ
-
+  char mean[1];//yêu cầu phải có mean đưa vào nhưng ko cần dùng đến nên để nó là xâu rỗng.
+  
   int rsize;
   //int num=0;
   while(btseln(btree, word,mean, 0, &rsize)==0)
@@ -22,7 +23,7 @@ void make_btree_soundex(BTA*btree)
       char*soundex_code=(char*)soundex(word);
 
       //printf("%s\t%s\n",soundex_code,word);
-      //chỗ này chỉ đưa từ vào thôi, đưa nghĩa vào load rất nặng nên thôi
+      //chỗ này chỉ đưa từ vào thôi, đưa nghĩa vào load rất nặng nên ko đưa vào.
       if(btchgr(btree_soundex,soundex_code)==0)
         btins(btree_soundex,word,"",0);
       else
